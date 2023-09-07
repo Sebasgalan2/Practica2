a0843b8262053c24d02603eac92f2b17dd8caf77
Sebasgalan2 <143724457+Sebasgalan2@users.noreply.github.com>
2023-09-07 09:51:34 -0500

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica2;
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Sebastian Galan Guerrero";
+        System.out.println("El nombre es"+nombre);
+        
+        int lado1= 70;
+        int lado2= 5;
+        
+        int arearectangulo = lado1*lado2;
+        System.out.println("El area de un rectangulo es"+arearectangulo);
+    }
+    
+}
